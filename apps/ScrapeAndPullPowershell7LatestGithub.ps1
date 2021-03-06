4bd002fb631add4bafa56a986490248f54ba4364
null-lex-null <57609591+null-lex-null@users.noreply.github.com>
2021-03-06 10:41:10 -0700

    Removed  a few redundant lines to make this run quicker, and verified works with latest version 7.1.2

--- apps/ScrapeAndPullPowershell7LatestGithub.ps1
+++ apps/ScrapeAndPullPowershell7LatestGithub.ps1
@@ -1,13 +1,11 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
 $uri = [uri]"https://github.com/PowerShell/PowerShell"
-$msi = (Invoke-WebRequest "$uri" -UseBasicParsing).links.href | Select-String -SimpleMatch 'msi'
-$msi = $msi | Sort-Object -Descending | Select-String -NotMatch 'preview'
-$msi = $msi | Select-String -SimpleMatch 'x64' | Select-Object -First 1
+$msi = (Invoke-WebRequest -Uri "$uri" -UseBasicParsing).links.href | select-string -SimpleMatch 'x64.msi' | select-string -NotMatch 'preview' | Sort-Object -Descending | Select-Object -First 1
 $msi = [string]$msi
 $msi = [uri]$msi
 $filename = $msi.Segments | Select-Object -Last 1
 $target = (Join-Path -Path $env:temp -ChildPath $filename)
-Invoke-WebRequest -Uri $msi -OutFile $target -UseBasicParsing
+(New-Object Net.WebClient).DownloadFile($($msi.AbsoluteUri), "$target")
 Unblock-File $target
 $MSIArguments = @(
     "/i"
