c73cb472b2617551af77e378fb8316f98d7cab6f
null-lex-null <57609591+null-lex-null@users.noreply.github.com>
2021-03-06 10:39:11 -0700

    Simple 7-zip installer

---
+++ apps/7-zip.ps1
@@ -0,0 +1,14 @@
+$fqdn = 'https://www.7-zip.org/'
+$uri = [uri]"$fqdn"
+try {
+    $file = (Invoke-WebRequest -Uri $uri.AbsoluteUri -UseBasicParsing).links.href | Select-String -SimpleMatch 'x64.exe' | Select-Object -First 1
+    $fileurl = $fqdn + $file
+    $fileurl = [uri]"$fileurl"
+    $filename = $fileurl.Segments | Select-Object -Last 1
+    $path = "$env:temp" + '\' + "$filename"
+    (New-Object Net.WebClient).DownloadFile("$fileurl", "$path")
+    IF (Test-Path -LiteralPath $path) { Start-Process "$path" -Verb RunAs -ArgumentList "/S" -Wait }  
+}
+catch {
+    $_
+}
