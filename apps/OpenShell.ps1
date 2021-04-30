ac417d342677f72b9e627c0f8bb1c732b7e3903b
null-lex-null <57609591+null-lex-null@users.noreply.github.com>
2021-04-30 14:40:33 -0700

    Modified arglist

--- apps/OpenShell.ps1
+++ apps/OpenShell.ps1
@@ -1,8 +1,10 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
 $uri = [uri]'https://github.com/Open-Shell/Open-Shell-Menu/releases'
 $request = Invoke-WebRequest -Uri $uri -UseBasicParsing
 $findlatest = ($request).links.href | Select-String -SimpleMatch 'OpenShellSetup' | Select-Object -First 1
 $download = [uri]('https://github.com' + $findlatest)
 $file = $download.segments | Select-Object -Last 1
 $path = Join-Path -Path "$env:temp" -ChildPath "$file"
-Invoke-WebRequest -Uri $download.AbsoluteUri -UseBasicParsing -OutFile $path
-Start-Process "$path" -ArgumentList "/qn ADDLOCAL=ClassicExplorer" -Wait -Verb RunAs
+(New-Object Net.WebClient).DownloadFile($($download.AbsoluteUri), "$path")
+#Invoke-WebRequest -Uri "$($download.AbsoluteUri)" -UseBasicParsing -OutFile $path
+Start-Process "$path" -ArgumentList "/qn ADDLOCAL=ClassicExplorer,StartMenu" -Wait -Verb RunAs
