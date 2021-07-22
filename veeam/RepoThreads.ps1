755430a1a9887c9a689a2fe0289416756ae5e71c
Alex Dewey <adewey@gmail.com>
2021-07-22 13:51:10 -0700

    Cleaning output to output one line per repo

--- veeam/RepoThreads.ps1
+++ veeam/RepoThreads.ps1
@@ -2,13 +2,11 @@ Invoke-Command -ScriptBlock {
     $repos = Get-VBRBackupRepository | Select-Object -Property *
     IF ($repos.count -gt '1') {
         foreach ($repo in $repos) {
-            Write-Host "$($repo.Name)"
-            Write-Host "Max Task Count: $($repo.Options.MaxTaskCount)"        
+            Write-Host "$($repo.Name):$($repo.Options.MaxTaskCount)"        
         }
     
     }
     IF ($repos.count -eq '1') {
-        Write-Host "$($repos.Name)"
-        Write-Host "Max Task Count: $($repos.Options.MaxTaskCount)" 
+        Write-Host "$($repos.Name):$($repos.Options.MaxTaskCount)"
     }
 }
