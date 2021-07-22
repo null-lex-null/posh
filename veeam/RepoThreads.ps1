d48d85d8379901bab430f08dab42a27df980c69c
Alex Dewey <adewey@gmail.com>
2021-07-22 13:48:39 -0700

    One little change to make it clean

--- veeam/RepoThreads.ps1
+++ veeam/RepoThreads.ps1
@@ -1,13 +1,14 @@
-$repos = Get-VBRBackupRepository | select-object -property *
-IF ($repos.count -gt '1') {
-    foreach ($repo in $repos) {
-        Write-Host "$($repo.Name)"
-        Write-Host "Max Task Count: $($repo.Options.MaxTaskCount)"        
-    }
+Invoke-Command -ScriptBlock {
+    $repos = Get-VBRBackupRepository | Select-Object -Property *
+    IF ($repos.count -gt '1') {
+        foreach ($repo in $repos) {
+            Write-Host "$($repo.Name)"
+            Write-Host "Max Task Count: $($repo.Options.MaxTaskCount)"        
+        }
     
+    }
+    IF ($repos.count -eq '1') {
+        Write-Host "$($repos.Name)"
+        Write-Host "Max Task Count: $($repos.Options.MaxTaskCount)" 
+    }
 }
-IF($repos.count -eq '1') {
-    Write-Host "$($repos.Name)"
-    Write-Host "Max Task Count: $($repos.Options.MaxTaskCount)" 
-}
-
